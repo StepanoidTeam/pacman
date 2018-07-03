b2caaafedaedc7aaeafc98bd53583dc29c45f762
Volodymyr Myshko <volodymyrm@wix.com>
2018-07-03 22:45:39 +0300

    fps counter component added

---
+++ src/components/fpsCounter.js
@@ -0,0 +1,28 @@
+export default class fpsCounter {
+	constructor({ ctx, position }) {
+		this.position = position;
+		this.prevStamp = null;
+		this.frameCount = 0;
+		this.prevFrameCount = 0;
+
+		this.ctx = ctx;
+		this.prevStamp = performance.now();
+	}
+	//todo: add enable/disable methods/gettersetters
+
+	update(timestamp = performance.now()) {
+		const delta = timestamp - this.prevStamp;
+		this.frameCount++;
+
+		if (delta > 1000) {
+			this.prevStamp = timestamp;
+			this.prevFrameCount = this.frameCount;
+			this.frameCount = 0;
+		}
+	}
+	draw() {
+		this.ctx.fillStyle = "black";
+		this.ctx.font = "22px serif";
+		this.ctx.fillText(this.prevFrameCount, ...this.position);
+	}
+}
