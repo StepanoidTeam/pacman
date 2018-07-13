8fafa4adef4196b99670eb6db43078acc4305f97
Volodymyr Myshko <volodymyrm@wix.com>
2018-07-14 00:18:13 +0300

    new sprites used

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,26 @@ import pacman from "./images/Pacman.png";
 import ghost1 from "./images/ghost-1.png";
 import ghost2 from "./images/ghost-2.png";
 
+import cake from "./images/cake.png";
+import berry1 from "./images/berries-1.png";
+import berry2 from "./images/berries-2.png";
+import berry3 from "./images/berries-3.png";
+
+import dot from "./images/dot-1.png";
+
+import wall from "./images/wall-1.png";
+
+const targetImgs = [cake, berry1, berry2, berry3];
+const followerImgs = [ghost1, ghost2, pacman];
+
 import GameLoop from "./gameLoop";
 
 import ClearScreen from "./components/clearScreen";
 import Pacman from "./components/pacman";
 import { Point, IComponent } from "./components/types";
+import Folllower from "./components/follower";
 
 import "./styles/index.less";
-import Folllower from "./components/follower";
 
 var canvas: HTMLCanvasElement = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
@@ -24,11 +36,13 @@ function batch(gcClass, count, getArgs) {
 
 const boundaries: Point = [1080, 720];
 
-const targets = batch(Pacman, 3, () => ({
+function getRandomPos() {}
+
+const targets = batch(Pacman, 5, () => ({
   ctx,
   position: [random(canvas.width - 100), random(canvas.height - 100)],
   velocity: [1 - 2 * random(1), 1 - 2 * random(1)],
-  image: sample([pacman]),
+  image: sample(targetImgs),
   size: [75, 75],
   boundaries
 }));
@@ -42,7 +56,7 @@ const components: Array<IComponent> = [
     ctx,
     position: [random(canvas.width - 100), random(canvas.height - 100)],
     velocity: [1 - 2 * random(1), 1 - 2 * random(1)],
-    image: sample([ghost1, ghost2]),
+    image: sample(followerImgs),
     size: [75, 75],
     boundaries,
     targets
