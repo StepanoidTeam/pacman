91dc3640c41998f676dea1938ebfeb814a627f22
Volodymyr Myshko <volodymyrm@wix.com>
2018-07-25 20:29:25 +0300

    rotation logic added based on matrices

--- src/components/vectors.ts
+++ src/components/vectors.ts
@@ -1,4 +1,4 @@
-import { Point } from "./types";
+import { Point, Matrix } from "./types";
 
 export function distance(pt1: Point, pt2: Point): number {
   return Math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2);
@@ -25,4 +25,23 @@ export function normalize(pt: Point): Point {
 
 export function floor(pt: Point): Point {
   return pt.map(x => Math.floor(x)) as Point;
+}
+
+export function dotProduct(a: Point, b: Point): number {
+  return a[0] * b[0] + a[1] * b[1];
+}
+
+export function mtxProduct(vector: Point, mtx: Matrix): Point {
+  return [dotProduct(vector, mtx[0]), dotProduct(vector, mtx[1])];
+}
+
+export function getRadians(degrees: number) {
+  return (degrees * Math.PI) / 180;
+}
+
+export function getRotationMtx(angle): Matrix {
+  const rads = getRadians(angle);
+  const { cos, sin } = Math;
+
+  return [[cos(rads), -sin(rads)], [sin(rads), cos(rads)]];
 }
