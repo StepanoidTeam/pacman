3bcace55c000b04f0b721392ebf19d5228eb32c9
Volodymyr Myshko <volodymyrm@wix.com>
2018-07-27 02:34:14 +0300

    ctx cleanup for clearScreen

--- src/components/clearScreen.ts
+++ src/components/clearScreen.ts
@@ -1,19 +1,23 @@
-import { IDraw, Point, IComponent } from "./types";
+import { Draw, Point, IComponent } from "./types";
 
 type Props = {
-  ctx: CanvasRenderingContext2D;
   boundaries: Point;
 };
 
-export default class ClearScreen implements IComponent {
-  constructor(public props: Props) {}
+export default class ClearScreen extends Draw implements IComponent {
+  constructor(public props: Props) {
+    super();
+  }
 
   update(timestamp: number) {}
 
   draw() {
-    const { ctx } = this.props;
-
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, this.props.boundaries[0], this.props.boundaries[1]);
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+    this.ctx.clearRect(
+      0,
+      0,
+      this.props.boundaries[0],
+      this.props.boundaries[1]
+    );
   }
 }
