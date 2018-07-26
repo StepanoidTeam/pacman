0740b33e9fb1924e08eb6f787da7e37b8d0a3ccf
Volodymyr Myshko <volodymyrm@wix.com>
2018-07-27 02:34:31 +0300

    canvas & ctx moved to config

--- src/components/config.ts
+++ src/components/config.ts
@@ -2,3 +2,6 @@ import { Point } from "./types";
 
 export const boundaries: Point = [1080, 720];
 export const tileSize: Point = [60, 60];
+
+export const canvas: HTMLCanvasElement = document.querySelector("#canvas");
+export const ctx = canvas.getContext("2d");
