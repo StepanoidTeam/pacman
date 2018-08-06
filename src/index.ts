ddab7a0b31cab7bba9e20e9e177ba16c9a106f7e
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-06 20:16:58 +0300

    gameobject logic usage + new brushes

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,10 @@ const followerImgs = ["ghost1", "ghost2", "pacman"];
 import GameLoop from "./gameLoop";
 
 import ClearScreen from "./components/clearScreen";
-import Pacman from "./components/pacman";
+import Pacman from "./components/mover";
 import { Point, IComponent } from "./components/types";
 import Folllower from "./components/follower";
-import Level from "./components/level";
+import Level, { LevelItem } from "./components/level";
 
 import "./styles/index.less";
 import { scalar, sub, floor } from "./components/vectors";
@@ -37,6 +37,9 @@ import level2 from "./levels/level-2.json";
 import level3 from "./levels/level-3.json";
 
 import { saveData } from "./saveData";
+import Bouncer from "./components/bouncer";
+import GameObject from "./components/GameObject";
+import dynamicClass from "./components/dynamicClass";
 
 const levels = [level1, level2, level3];
 
@@ -68,28 +71,42 @@ const rotators = batch(Rotator, 20, () => ({
   angle: 1 // random(1 / 4, 1 / 2, true)
 }));
 
+var testProps = {
+  position: getRandomPos(),
+  velocity: [1 - 2 * random(1), 1 - 2 * random(1)] as Point,
+  image: "ghost1",
+  size: tileSize,
+  boundaries
+};
+
+var testEntity = new GameObject(testProps, Bouncer);
+
+console.log(testEntity);
+
 const components: Array<IComponent> = [
   new ClearScreen({ boundaries }),
   level,
-  ...followers,
-  ...targets,
-  ...rotators
+  //...followers,
+  //...targets,
+  //...rotators
+  testEntity
 ];
 
 new GameLoop({ components }).start();
 
 //controls
-let drawItems = [
+let drawItems: Array<(...agrs) => LevelItem> = [
   position => ({
-    type: "Bouncer",
+    classNames: ["Bouncer", "Rotator"],
     props: {
       position: position,
       size: tileSize,
-      image: "cake"
+      image: "cake",
+      angle: random(-3, 3)
     }
   }),
   position => ({
-    type: "Bouncer",
+    classNames: ["Bouncer"],
     props: {
       position: position,
       size: tileSize,
@@ -97,11 +114,21 @@ let drawItems = [
     }
   }),
   position => ({
-    type: "Sprite",
+    classNames: ["Sprite"],
     props: {
       position: position,
       size: tileSize,
       image: "wall"
+    }
+  }),
+  position => ({
+    classNames: ["Bouncer", "Mover"],
+    props: {
+      position: position,
+      size: tileSize,
+      image: "ghost2",
+      velocity: [1, 1],
+      boundaries: boundaries
     }
   })
 ];
@@ -116,7 +143,15 @@ canvas.addEventListener("click", event => {
 
   let position = alignToGrid(offset, tileSize[0]);
 
-  level.addItem(drawItems[0](position));
+  let currentItem2add = drawItems[0](position);
+
+  //todo: duplicate in level.ts
+  let types = currentItem2add.classNames.map(dynamicClass);
+
+  //const $class = dynamicClass(item.type);
+  // const component = new $class(item.props);
+
+  level.addItem(new GameObject(currentItem2add.props, ...types));
 });
 
 document.body.addEventListener("keypress", event => {
@@ -158,6 +193,7 @@ const ctrls = [
     action: () => {
       const prev = drawItems.pop();
       drawItems.unshift(prev);
+      document.title = prev.name;
     }
   },
   {
