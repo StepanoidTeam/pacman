3b18491f8141bd32234635712828dc7f5ff43946
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-06 20:15:51 +0300

    gameobject made serializable

--- src/components/GameObject.ts
+++ src/components/GameObject.ts
@@ -1,17 +1,26 @@
 import { IComponent } from "./types";
 
+export type Props = {
+  //children: Array<GameObject>;
+};
+
 //todo: create unity-like multi-component
 export default class GameObject implements IComponent {
-  components: Array<IComponent> = [];
+  gameObjects: Array<IComponent>;
+  classNames: Array<string>;
 
   constructor(public props, ...$classes) {
-    this.components = $classes.map($class => new $class(props));
+    this.gameObjects = $classes.map($class => new $class(props));
+
+    //for serialization purposes
+    this.classNames = $classes.map($class => $class.name);
+    //this.props.children = [];
   }
 
   draw() {
-    this.components.forEach(c => c.draw());
+    this.gameObjects.forEach(c => c.draw());
   }
   update(timestamp: number) {
-    this.components.forEach(c => c.update(timestamp));
+    this.gameObjects.forEach(c => c.update(timestamp));
   }
 }
